4292ac4d7ff45ef3738f8d8d78754864982281b3
jmears98 <jmears98@gmail.com>
2024-03-25 20:40:21 -0400

    Started this lovely project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
