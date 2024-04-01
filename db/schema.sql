bb972a2edda7bfe4979047fd6ace70f4e8f7d561
jmears98 <jmears98@gmail.com>
2024-04-01 18:26:31 -0400

    added sign up page

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,16 @@ CREATE DATABASE yujojo_db;
 
 USE DATABASE yujojo_db;
 
-CREATE TABLE user (
-  username VARCHAR(15) NOT NULL,
-  password VARCHAR(15) NOT NULL,
-  PRIMARY KEY (username)
-);
+-- CREATE TABLE user (
+--   username VARCHAR(15) NOT NULL,
+--   password VARCHAR(15) NOT NULL,
+--   PRIMARY KEY (username)
+-- );
 
-CREATE TABLE post (
-  title  VARCHAR(50),
-  text VARCHAR(500),
-  image BLOB,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+-- CREATE TABLE post (
+--   title  VARCHAR(50),
+--   text VARCHAR(500),
+--   image BLOB,
+--   date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
   
-);
+-- );
--- server.js
+++ server.js
@@ -1,43 +1,3 @@
-// // imports
-// const express = require('express');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-// const sequelize = require('./config/connections');
-// const models = require('./models');
-
-// const app = express();
-
-// app.engine('handlebars', exphbs.engine());
-// app.set('view engine', 'handlebars');
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// const PORT = process.env.PORT || 3001;
-
-// app.get('/', (req, res) => {
-//   res.render('home');
-// });
-
-// app.get('/contact', (req, res) => {
-//   res.render('contact');
-// });
-
-// app.get('./users', async (req, res) => {
-//   const userData = await models.User.findAll({});
-//   return res.json(userData);
-// });
-
-// app.post('./users', async (req, res) => {
-//   const userData = await models.User.create({ ...req.body });
-//   return res.json(userData);
-// });
-
-// (async () => {
-//   await sequelize.sync();
-//   app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
-// })();
-
 // imports
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -47,7 +7,7 @@ const models = require('./models');
 
 const app = express();
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -77,5 +37,3 @@ app.post('/users', async (req, res) => {
   await sequelize.sync();
   app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 })();
-
-
